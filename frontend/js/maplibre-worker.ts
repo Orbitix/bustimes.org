e1487ab89e02309ff00b0c638ed512a0ecd3b325
Josh Goodwin <j@joshuagoodw.in>
2026-08-27 17:36:55 +0100

    Revert "maplibre gl js version 6"
    This reverts commit 428ae3fb08bac8c63086c988e0f4f2a6b6f64501.

--- frontend/js/maplibre-worker.ts
+++
@@ -1,4 +0,0 @@
-import * as maplibreGlWorker from "maplibre-gl/dist/maplibre-gl-worker.mjs";
-
-// @ts-expect-error prevent tree-shaking of the worker's side effects
-globalThis.__maplibreGlWorker = maplibreGlWorker;
